91f8c22c4b40d1ab237cbb1cc7b6001b97539284
ingjuanpa <ingjuanpabloduran@gmail.com>
2024-10-23 21:51:10 -0500

    Validando fecha de Nac

--- AñosCumplido.py
+++ AñosCumplido.py
@@ -16,39 +16,46 @@ fecha_nacimiento_str = input(f"{nombre} Por favor, seguidamente ingresa tu fecha
 
 # Convertir la cadena de texto ingresada en un objeto de tipo datetime
 
-
 try:
     fecha_nacimiento = datetime.strptime(fecha_nacimiento_str, "%d/%m/%Y")
     print(f"{nombre} Tu fecha de nacimiento es: {fecha_nacimiento.date()}")
 except ValueError:
     print("El formato de la fecha ingresada es incorrecto. Asegúrate de usar el formato DD/MM/AAAA.")
 
-# Guardar la fecha actual en la variable FechaActual
-FechaActual = datetime.today().date()
+# Obtener la fecha actual para validar
+fecha_actual = datetime.now()
 
- # Calcular la edad exacta en años
-edad = FechaActual.year - fecha_nacimiento.year - ((FechaActual.month, FechaActual.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
-print(f"{nombre} Tienes {edad} años cumplidos.")
+# Comparar la fecha ingresada  de nacimiento con la fecha actual
+if fecha_nacimiento < fecha_actual:
 
-dias = FechaActual.day
+    # Guardar la fecha actual en la variable FechaActual
+    FechaActual = datetime.today().date()
 
-meses = 11 + (FechaActual.month - fecha_nacimiento.month)
+    # Calcular la edad exacta en años
+    edad = FechaActual.year - fecha_nacimiento.year - ((FechaActual.month, FechaActual.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+    print(f"{nombre} Tienes {edad} años cumplidos.")
 
-DifmesfechaNac = FechaActual.month - fecha_nacimiento.month - ((FechaActual.day) < (fecha_nacimiento.day))
+    dias = FechaActual.day
 
-DifdiasfechaNac = FechaActual.day - fecha_nacimiento.day
+    meses = 11 + (FechaActual.month - fecha_nacimiento.month)
 
-if DifmesfechaNac < 0 and DifdiasfechaNac == 0:
-    print(f"mas exactamente tienes {edad} años, con {meses+1} meses y cero dias")
-elif DifmesfechaNac == 0 and DifdiasfechaNac < 0:
-    # Obtener el último día del mes de la fecha de nacimiento
-    ultimo_dia_mes = calendar.monthrange(fecha_nacimiento.year, fecha_nacimiento.month)[1]
-    # Calcular los días restantes hasta el fin del mes
-    dias_rest = ultimo_dia_mes - fecha_nacimiento.day
-    print(f"mas exactamente tienes {edad} años, con cero meses y {dias+dias_rest} dias")
-elif DifmesfechaNac < 0:
-    print(f"mas exactamente tienes {edad} años, con {meses} meses y {dias} dias")
-elif DifdiasfechaNac < 0:
-    print(f"mas exactamente tienes {edad} años, con {DifmesfechaNac} meses y {dias} dias")
+    DifmesfechaNac = FechaActual.month - fecha_nacimiento.month - ((FechaActual.day) < (fecha_nacimiento.day))
+
+    DifdiasfechaNac = FechaActual.day - fecha_nacimiento.day
+
+    if DifmesfechaNac < 0 and DifdiasfechaNac == 0:
+        print(f"mas exactamente tienes {edad} años, con {meses+1} meses y cero dias")
+    elif DifmesfechaNac == 0 and DifdiasfechaNac < 0:
+        # Obtener el último día del mes de la fecha de nacimiento
+        ultimo_dia_mes = calendar.monthrange(fecha_nacimiento.year, fecha_nacimiento.month)[1]
+        # Calcular los días restantes hasta el fin del mes
+        dias_rest = ultimo_dia_mes - fecha_nacimiento.day
+        print(f"mas exactamente tienes {edad} años, con cero meses y {dias+dias_rest} dias")
+    elif DifmesfechaNac < 0:
+        print(f"mas exactamente tienes {edad} años, con {meses} meses y {dias} dias")
+    elif DifdiasfechaNac < 0:
+        print(f"mas exactamente tienes {edad} años, con {DifmesfechaNac} meses y {dias} dias")
+    else:
+        print(f"mas exactamente tienes {edad} años, con {DifmesfechaNac} meses y {DifdiasfechaNac} dias")
 else:
-    print(f"mas exactamente tienes {edad} años, con {DifmesfechaNac} meses y {DifdiasfechaNac} dias")
+    print("LO SIENTO LA FECHA DE NACIMIENTO DEBE SER MENOR A LA FECHA DE HOY")
